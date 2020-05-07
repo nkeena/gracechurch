27f1db4c126b94cb87444e1e786baae3af0832d5
Neil Keena <nkeena1@gmail.com>
2020-05-06 18:14:28 -0700

    update mailing address

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -34,8 +34,8 @@
                 <div class="md:w-1/2 w-full px-2 md:mb-0 mb-4">
                     <h2 class="text-2xl text-grey-darker mb-4">Mailing Address</h2>
                     <address class="text-grey-dark text-xl">
-                        Post Office Box 760848<br />
-                        San Antonio, TX 78245
+                        5415 Bandera Road suite 501<br />
+                        San Antonio, TX 78238
                     </address>
                 </div>
 
